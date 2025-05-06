459d726eb7968ee4851ee1d3b2a541c045762778
HasiruChamodya <commenty460@gmail.com>
2025-05-06 16:10:18 +0530

    initialize commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 int lightSensorPin = A0; // Pin connected to the light sensor
 int lightSensorValue ; // Variable to store the light sensor value
 int delayTime = 1000; // Delay time in milliseconds
+int greenLedPin = 2; // Pin connected to the green LED
+int redLedPin = 3; // Pin connected to the red LED
 void setup() {
   // put your setup code here, to run once:
 
 pinMode(lightSensorPin, INPUT); // Set the light sensor pin as input
+pinMode(greenLedPin, OUTPUT); // Set the green LED pin as output
+pinMode(redLedPin, OUTPUT); // Set the red LED pin as output
 Serial.begin(9600); // Initialize serial communication at 9600 bps
 }
 
@@ -19,5 +23,10 @@ Serial.print(lightSensorValue); // Print the light sensor value
 Serial.println(); // Print a new line
 delay(delayTime); // Wait for 1 second before the next reading
 
+if(lightSensorValue < 600){
+digitalWrite(greenLedPin, HIGH); // Turn on the green LED
+}
+else 
+digitalWrite(redLedPin, HIGH); // Turn on the red LED
 }
 
